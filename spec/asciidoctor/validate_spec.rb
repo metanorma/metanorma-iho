72c4ab1870e391a4ed6b0a3849c47b4723151406
Тараскин Михаил <MITaraskin@domclick.ru>
2020-10-09 21:22:22 +0300

    metanorma/metanorma-standoc#225 add error file generation test

--- spec/asciidoctor/validate_spec.rb
+++ spec/asciidoctor/validate_spec.rb
@@ -1,6 +1,26 @@
 require "spec_helper"
 
 RSpec.describe Asciidoctor::IHO do
+  context "when xref_error.adoc compilation" do
+    around do |example|
+      FileUtils.rm_f "spec/assets/xref_error.err"
+      example.run
+      Dir["spec/assets/xref_error*"].each do |file|
+        next if file.match?(/adoc$/)
+
+        FileUtils.rm_f(file)
+      end
+    end
+
+    it "generates error file" do
+      expect do
+        Metanorma::Compile
+          .new
+          .compile("spec/assets/xref_error.adoc", type: "iho")
+      end.to(change { File.exist?("spec/assets/xref_error.err") }
+              .from(false).to(true))
+    end
+  end
 
   it "warns about missing workgroup" do
   FileUtils.rm_f "test.err"
