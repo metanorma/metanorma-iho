f2ab9bc049b8a25bf7076549d4b49fb1019c93a1
Nick Nicholas <opoudjis@gmail.com>
2021-11-30 01:21:59 +1100

    version bump

--- lib/metanorma/iho/version.rb
+++ lib/metanorma/iho/version.rb
@@ -1,5 +1,5 @@
 module Metanorma
   module IHO
-    VERSION = "0.4.2".freeze
+    VERSION = "0.4.3".freeze
   end
 end
--- spec/metanorma/processor_spec.rb
+++ spec/metanorma/processor_spec.rb
@@ -3,13 +3,12 @@ require "metanorma"
 require "fileutils"
 
 RSpec.describe Metanorma::IHO::Processor do
-
   registry = Metanorma::Registry.instance
   registry.register(Metanorma::IHO::Processor)
 
-  let(:processor) {
+  let(:processor) do
     registry.find_processor(:iho)
-  }
+  end
 
   it "registers against metanorma" do
     expect(processor).not_to be nil
@@ -17,7 +16,7 @@ RSpec.describe Metanorma::IHO::Processor do
 
   it "registers output formats against metanorma" do
     expect(processor.output_formats.sort.to_s).to be_equivalent_to <<~"OUTPUT"
-    [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
+      [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
     OUTPUT
   end
 
@@ -27,52 +26,52 @@ RSpec.describe Metanorma::IHO::Processor do
 
   it "generates IsoDoc XML from a blank document" do
     input = <<~"INPUT"
-    #{ASCIIDOC_BLANK_HDR}
+      #{ASCIIDOC_BLANK_HDR}
     INPUT
 
     output = xmlpp(strip_guid(<<~"OUTPUT"))
-    #{BLANK_HDR}
-<sections/>
-</iho-standard>
+          #{BLANK_HDR}
+      <sections/>
+      </iho-standard>
     OUTPUT
 
-    expect(xmlpp(strip_guid(processor.input_to_isodoc(input, nil)))).to be_equivalent_to output
+    expect(xmlpp(strip_guid(processor
+      .input_to_isodoc(input, nil))))
+      .to be_equivalent_to output
   end
 
   it "generates HTML from IsoDoc XML" do
     FileUtils.rm_f "test.xml"
     input = <<~"INPUT"
-    <iho-standard xmlns="http://riboseinc.com/isoxml">
-      <sections>
-        <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
-          <term id="J">
-          <name>1.1.</name>
-            <preferred>Term2</preferred>
-          </term>
-        </terms>
-      </sections>
-    </iho-standard>
+      <iho-standard xmlns="http://riboseinc.com/isoxml">
+        <sections>
+          <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
+            <term id="J">
+            <name>1.1.</name>
+              <preferred>Term2</preferred>
+            </term>
+          </terms>
+        </sections>
+      </iho-standard>
     INPUT
 
     output = xmlpp(<<~"OUTPUT")
-    <main class="main-section">
-      <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-      <p class="zzSTDTitle1"></p>
-      <div id="H">
-        <h1 id="toc0">1.&#xA0; Terms, Definitions, Symbols and Abbreviated Terms</h1>
-        <h2 class="TermNum" id="J">1.1.&#xA0;<p class="Terms" style="text-align:left;">Term2</p></h2>
-      </div>
-    </main>
+      <main class="main-section">
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+        <p class="zzSTDTitle1"></p>
+        <div id="H">
+          <h1 id="toc0">1.&#xA0; Terms, Definitions, Symbols and Abbreviated Terms</h1>
+          <p class='Terms' style='text-align:left;' id='J'><strong>1.1.</strong>&#xA0;Term2</p>
+        </div>
+      </main>
     OUTPUT
 
     processor.output(input, "test.xml", "test.html", :html)
 
     expect(
-      xmlpp(File.read("test.html", encoding: "utf-8").
-      gsub(%r{^.*<main}m, "<main").
-      gsub(%r{</main>.*}m, "</main>"))
+      xmlpp(File.read("test.html", encoding: "utf-8")
+      .gsub(%r{^.*<main}m, "<main")
+      .gsub(%r{</main>.*}m, "</main>")),
     ).to be_equivalent_to output
-
   end
-
 end
