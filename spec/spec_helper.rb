fa3b5fb5750d094c3971f4206f360be93e205297
Nick Nicholas <opoudjis@gmail.com>
2022-11-15 03:51:22 +1100

    remove rexml from rspec: https://github.com/metanorma/isodoc/issues/260

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ require "rspec/matchers"
 require "equivalent-xml"
 require "htmlentities"
 require "metanorma"
-require "rexml/document"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -50,7 +49,17 @@ def htmlencode(xml)
 end
 
 def xmlpp(xml)
-  Nokogiri::XML(xml).to_xml(indent: 2, encoding: "UTF-8")
+ xsl = <<~XSL
+    <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+      <xsl:output method="xml" encoding="UTF-8" indent="yes"/>
+      <xsl:strip-space elements="*"/>
+      <xsl:template match="/">
+        <xsml:copy-of select="."/>
+      </xsl:template>
+    </xsl:stylesheet>
+  XSL
+  Nokogiri::XSLT(xsl).transform(Nokogiri::XML(xml))
+    .to_xml(indent: 2, encoding: "UTF-8")
 end
 
 ASCIIDOC_BLANK_HDR = <<~"HDR".freeze
