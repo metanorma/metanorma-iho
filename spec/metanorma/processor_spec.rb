86d55e862080d690fab11aa4c2999471ef658366
Nick Nicholas <opoudjis@gmail.com>
2023-07-29 00:00:36 +1000

    https://github.com/metanorma/metanorma-ogc/issues/567

--- spec/metanorma/processor_spec.rb
+++ spec/metanorma/processor_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Metanorma::IHO::Processor do
   end
 
   it "registers output formats against metanorma" do
-    expect(processor.output_formats.sort.to_s).to be_equivalent_to <<~"OUTPUT"
+    expect(processor.output_formats.sort.to_s).to be_equivalent_to <<~OUTPUT
       [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
     OUTPUT
   end
@@ -42,10 +42,11 @@ RSpec.describe Metanorma::IHO::Processor do
 
   it "generates HTML from IsoDoc XML" do
     FileUtils.rm_f "test.xml"
-    input = <<~"INPUT"
+    input = <<~INPUT
       <iho-standard xmlns="http://riboseinc.com/isoxml">
         <sections>
-          <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
+          <terms id="H" obligation="normative" displayorder="1">
+            <title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
             <term id="J">
             <name>1.1.</name>
               <preferred>Term2</preferred>
@@ -55,10 +56,9 @@ RSpec.describe Metanorma::IHO::Processor do
       </iho-standard>
     INPUT
 
-    output = xmlpp(strip_guid(<<~"OUTPUT"))
+    output = xmlpp(strip_guid(<<~OUTPUT))
       <main class="main-section">
         <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-        <p class="zzSTDTitle1"></p>
         <div id="H">
           <h1 id="_">1.&#xA0; Terms, Definitions, Symbols and Abbreviated Terms</h1>
           <p class='Terms' style='text-align:left;' id='J'><strong>1.1.</strong>&#xa0;Term2</p>
