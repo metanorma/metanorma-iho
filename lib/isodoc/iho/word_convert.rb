1f5439b6a5ef0c95647491413195e59fb2a03e30
Nick Nicholas <opoudjis@gmail.com>
2024-04-08 16:25:39 +1000

    version bump

--- lib/isodoc/iho/word_convert.rb
+++ lib/isodoc/iho/word_convert.rb
@@ -20,20 +20,6 @@ module IsoDoc
         section_break(body)
       end
 
-      #       def make_body2(body, docxml)
-      #         body.div **{ class: "WordSection2" } do |div2|
-      #           boilerplate docxml, div2
-      #           preface_block docxml, div2
-      #           abstract docxml, div2
-      #           foreword docxml, div2
-      #           introduction docxml, div2
-      #           preface docxml, div2
-      #           acknowledgements docxml, div2
-      #           div2.p { |p| p << "&nbsp;" } # placeholder
-      #         end
-      #         section_break(body)
-      #       end
-
       def authority_cleanup(docxml)
         super
         docxml.xpath("//div[@class = 'boilerplate-feedback']/p").each do |p|
--- lib/metanorma/iho/version.rb
+++ lib/metanorma/iho/version.rb
@@ -1,5 +1,5 @@
 module Metanorma
   module IHO
-    VERSION = "0.9.5".freeze
+    VERSION = "0.9.6".freeze
   end
 end
