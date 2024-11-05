c1266d648248ff06c87bb49c7cbc07375474bbb7
Nick Nicholas <opoudjis@gmail.com>
2024-11-05 19:20:54 +1100

    xrefs refactor: https://github.com/metanorma/isodoc/issues/606

--- lib/isodoc/iho/init.rb
+++ lib/isodoc/iho/init.rb
@@ -10,7 +10,7 @@ module IsoDoc
       end
 
       def xref_init(lang, script, _klass, labels, options)
-        html = PresentationXMLConvert.new(language: lang, script: script)
+        html = HtmlConvert.new(language: lang, script: script)
         @xrefs = Xref.new(lang, script, html, labels, options)
       end
 
