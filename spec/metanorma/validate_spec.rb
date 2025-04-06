a078619faca184ca20a96156e076614a2002d83b
Nick Nicholas <opoudjis@gmail.com>
2025-04-06 12:58:25 +1000

    schema testing: https://github.com/metanorma/metanorma/issues/437

--- spec/metanorma/validate_spec.rb
+++ spec/metanorma/validate_spec.rb
@@ -36,4 +36,18 @@ RSpec.describe Metanorma::Iho do
     expect(File.read("test.err.html"))
       .to include("Missing workgroup attribute for document")
   end
+
+  it "validates document against Metanorma XML schema" do
+    Asciidoctor.convert(<<~"INPUT", backend: :iho, header_footer: true)
+      = A
+      X
+      :docfile: test.adoc
+      :no-pdf:
+  
+      [align=mid-air]
+      Para
+    INPUT
+    expect(File.read("test.err.html"))
+      .to include('value of attribute "align" is invalid; must be equal to')
+  end
 end
