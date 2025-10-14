f9ec5ce68adfe027eb39a817fbe0fcbb39119ee7
Nick Nicholas <opoudjis@gmail.com>
2025-10-15 00:09:58 +1100

    merge user-specified i18n with flavour i18n: https://github.com/metanorma/metanorma-pdfa/issues/7

--- lib/isodoc/iho/init.rb
+++ lib/isodoc/iho/init.rb
@@ -17,8 +17,8 @@ module IsoDoc
       def i18n_init(lang, script, locale, i18nyaml = nil)
         @i18n = I18n.new(
           lang, script, locale: locale,
-                        i18nyaml: i18nyaml ||
-          Metanorma::Iho.configuration.i18nyaml || @i18nyaml
+                        i18nyaml: [i18nyaml,
+          Metanorma::Iho.configuration.i18nyaml, @i18nyaml]
         )
       end
 
